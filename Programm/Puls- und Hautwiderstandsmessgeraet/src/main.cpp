623a09d17c3c80f1a2fe96a33bf4ce7caa930853
Alx <alxdoesstuff@gmail.com>
2024-11-14 10:47:19 +0100

    Deno

--- Programm/Puls- und Hautwiderstandsmessgeraet/src/main.cpp
+++ Programm/Puls- und Hautwiderstandsmessgeraet/src/main.cpp
@@ -1,10 +1,34 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+
+const char *SSID = "ssid"; //Mit Netzwerk-SSID ersetzen
+const char *PASSWORD = "password"; //Mit Netzwerk-passwort ersetzen
+const char *SERVERURL = "localhost:80";
+
 
 void setup() {
-  WiFi.mode(WIFI_STA);
+  Serial.begin(115200); //Seriellen Monitor beginnen
+      WiFi.begin(SSID, PASSWORD); //Mit WLAN-Netzwerk verbinden
+      //Warten, bis mit WLAN verbunden wurde
+      while (WiFi.status() != WL_CONNECTED)
+      {
+            delay(1000);
+            Serial.println("Verbinde mit WLAN...");
+      }
+      Serial.println("Mit WLAN verbunden  ");
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  long currTime = millis(); //Wie lang der ESP schon läuft in ms
+  int BPM = 0; //Derzeitige Herzfrequenz
+  int skinResistance = 0; //Derzeitiger Hautwiderstand
+
+  //Daten senden per HTTP request (Die Daten werden später aus der URL geschlossen)
+  HTTPClient http; 
+  String url = String(SERVERURL) + "/bpm/" + String(BPM) + "/skinResistance/" + String(skinResistance)+"/currentTime/" + String(currTime); //Request aufbauen
+  http.begin(url);
+  int httpCode = http.GET();
 }
