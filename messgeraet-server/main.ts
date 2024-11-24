78d104bca940c1a8a68fa126c13612f049285c13
Alx <alxdoesstuff@gmail.com>
2024-11-24 12:00:24 +0100

    Deno Server

--- messgeraet-server/main.ts
+++ messgeraet-server/main.ts
@@ -1,22 +1,44 @@
-Deno.serve({ port: 8000, hostname: "0.0.0.0" }, (_req) => {
-  let requestURL = _req.url; //Store request URL
-  let URLSplit = requestURL.split("/");
+const TABLE_DIRECTORY = "./data/"; //Ordner, in dem die Tabelle gespeichert wird
+Deno.serve({ port: 8000, hostname: "0.0.0.0" }, handler); //Server auf 8000 hosten
+//Request Handler
+async function handler(_req: Request){
+  const requestURL = _req.url; //Request-URL speichern
+  const URLSplit = requestURL.split("/"); //Request-URL in bestandteile trennen
   //Unterscheide zwischen Verbindungsankündigung, Daten und Müll
   if(URLSplit[3]){
     if(URLSplit[3] == "connected" && URLSplit.length == 6){
-      let connectTimeParsed = parseInt(URLSplit[4]); //Die Zeit, seitdem der Verbundene ESP läuft in ms (int)
-      let connectIDParsed = parseInt(URLSplit[5]); //Die ID des verbundenen ESPs in ms (int)
-      console.log("New ESP connected. ID: " + connectIDParsed + ", running for: " + connectTimeParsed);
-    }else if(URLSplit[3] == "data" && URLSplit.length == 8){
-      let dataTimeParsed = parseInt(URLSplit[6]); //Die Zeit, seitdem der Verbundene ESP läuft in ms (int)
-      let dataIDParsed = parseInt(URLSplit[7]); //Die ID des ESPs, der die Daten in ms (int)
-      let pulseParsed = parseInt(URLSplit[4]); 
-      let skinResistanceParsed = parseInt(URLSplit[5]); 
-      console.log("Data recieved. ID: " + dataIDParsed + ", data at time: " + dataTimeParsed + ". Pulse: "+ pulseParsed + " Skin Resistance: "+skinResistanceParsed);
-    }else{  
-      console.log("Incorrect URL: "+requestURL); 
+      const connectIDParsed = parseInt(URLSplit[5]); //Die ID des verbundenen ESPs 
+      //Datum
+      const realTime = new Date();
+      const DD = ("0" + realTime.getDate()).slice(-2);
+      const MM = ("0" + (realTime.getMonth() + 1)).slice(-2);
+      const YYYY = realTime.getFullYear();
+      const hh = realTime.getHours();
+      const mm = realTime.getMinutes();
+      const ss = realTime.getSeconds();
+      //Tabelle erstellen mit Name: ID + Datum und Uhrzeit und Erste Zeile: Zeit, Puls und Widerstand
+      const newTableName = TABLE_DIRECTORY + connectIDParsed + "--"+DD+"-"+MM+"-"+YYYY+"-"+hh+"-"+mm+"-"+ss+".csv" //Name der neuen Tabelle
+      await Deno.writeTextFile(newTableName, "Zeit,Puls,Widerstand\n"); //Neue Tabelle erstellen
+      await Deno.writeTextFile(TABLE_DIRECTORY + connectIDParsed.toString(), newTableName); //Die neue Tabelle für das ID eintragen
+    }
+    else if(URLSplit[3] == "data" && URLSplit.length == 8){
+      //Daten aus der URL entlesen
+      const dataTimeParsed = parseInt(URLSplit[6]); //Die Zeit, seitdem der Verbundene ESP läuft in ms
+      const dataIDParsed = parseInt(URLSplit[7]); //Die ID des ESPs, der die Daten sendet
+      const pulseParsed = parseInt(URLSplit[4]);  //Gemessener Puls
+      const skinResistanceParsed = parseInt(URLSplit[5]); //Gemessener Hautwiderstand
+      //Tabelle für das ID finden
+      const tableNameRaw = await Deno.readFile(TABLE_DIRECTORY + dataIDParsed); //Tabellenregister lesen als Bytes
+      const decoder = new TextDecoder("utf-8"); //UTF-8 byte decoder
+      const tableName = decoder.decode(tableNameRaw); //Decodierter Tabellenname
+      //Daten in die entsprechende Tabelle eintragen
+      await Deno.writeTextFile(tableName, dataTimeParsed + "," + pulseParsed + "," + skinResistanceParsed +"\n", { append: true });
+    }
+    else{  
+      console.log("Incorrect URL: "+requestURL); //Falsche URL-formate zur Konsole ausgeben
+      return new Response("404"); //404 antworten
     }
     
-  }  return new Response("OK"); //Auf alle requests OK als response 
-});
+  }  return new Response("OK"); //Auf alle korrekten Requests OK antworten 
+}
 
